7bfc1039215132f43bd9627d4b85db6eb24b145a
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-02-04 19:39:29 +0530

    Update PyroHelperz.py

--- helpers/PyroHelperz.py
+++ helpers/PyroHelperz.py
@@ -1,39 +1,37 @@
-from pyrogram.types import Message, User
-from pyrogram import Client, enums
+from pyrogram import Client, Filters
 
 async def get_ub_chats(
     client: Client,
     chat_types: list = [
-        enums.ChatType.GROUP,
-        enums.ChatType.SUPERGROUP,
-        enums.ChatType.CHANNEL,
+        "group",
+        "supergroup",
+        "channel",
     ],
     is_id_only=True,
 ):
     ub_chats = []
-    async for dialog in client.get_dialogs():
-        if dialog.chat.type in chat_types:
+    async for chat in client.iter_dialogs():
+        if chat.chat.type in chat_types:
             if is_id_only:
-                ub_chats.append(dialog.chat.id)
+                ub_chats.append(chat.chat.id)
             else:
-                ub_chats.append(dialog.chat)
+                ub_chats.append(chat.chat)
         else:
             continue
     return ub_chats
 
-def ReplyCheck(message: Message):
+def reply_check(message: Message):
     reply_id = None
 
     if message.reply_to_message:
-        reply_id = message.reply_to_message.id
+        reply_id = message.reply_to_message.message_id
 
     elif not message.from_user.is_self:
-        reply_id = message.id
+        reply_id = message.message_id
 
     return reply_id
 
-
-def SpeedConvert(size):
+def speed_convert(size):
     power = 2 ** 10
     zero = 0
     units = {0: "", 1: "Kbit/s", 2: "Mbit/s", 3: "Gbit/s", 4: "Tbit/s"}
@@ -42,16 +40,13 @@ def SpeedConvert(size):
         zero += 1
     return f"{round(size, 2)} {units[zero]}"
 
-
-def GetFromUserID(message: Message):
+def get_from_user_id(message: Message):
     return message.from_user.id
 
-
-def GetChatID(message: Message):
+def get_chat_id(message: Message):
     return message.chat.id
 
-
-def GetUserMentionable(user: User):
+def get_user_mentionable(user: User):
     if user.username:
         username = "@{}".format(user.username)
     else:
@@ -60,6 +55,6 @@ def GetUserMentionable(user: User):
         else:
             name_string = "{}".format(user.first_name)
 
-        username = "<a href='tg://user?id={}'>{}</a>".format(user.id, name_string)
+        username = f"<a href='tg://user?id={user.id}'>{name_string}</a>"
 
     return username
