755fa0510bfe027a00e8b66271de6276d5bf9dda
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-02-23 00:21:41 +0530

    Update sangmata.py

--- handlers/sangmata.py
+++ handlers/sangmata.py
@@ -1,39 +1,64 @@
-	
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import asyncio
+
+from pyrogram import *
 from pyrogram import filters
+from pyrogram.errors import YouBlockedUser
+from pyrogram.types import *
 
 from helpers.basic import edit_or_reply
-from pyrogram.errors import RPCError
-from pyrogram import * 
-from pyrogram.types import *
-from handlers.help import *
-from config import SUDO_USERS as AFS
+from utils.misc import extract_user
 
-@Client.on_message(filters.user(AFS) & filters.command(['sg', "sangmata"], ["."])) 
-@Client.on_message(filters.command(['sg', "sangmata"], ["."]) & filters.me)
+from handlers.help import *
+
+
+@Client.on_message(filters.command(["sg", "sangmata"], ["."]))
 async def sg(client: Client, message: Message):
-    lol = await edit_or_reply(message, "Processing please wait")
-    if not message.reply_to_message:
-        await lol.edit("reply to any message")
-    reply = message.reply_to_message
-    if not reply.text:
-        await lol.edit("reply to any text message")
-    chat = message.chat.id
-    await reply.forward("@SangMataInfo_bot")
-    await asyncio.sleep(2)
-    async for opt in client.iter_history("@SangMataInfo_bot", limit=3):
-        hmm = opt.text
-        if hmm.startswith("Forward"):
-            await lol.edit("Can you kindly disable your privacy settings for good")
+    args = await extract_user(message)
+    lol = await edit_or_reply(message, "`Processing...`")
+    if args:
+        try:
+            user = await client.get_users(args)
+        except Exception:
+            return await lol.edit(f"`Please specify a valid user!`")
+    bot = "SangMataInfo_bot"
+    try:
+        await client.send_message(bot, f"/search_id {user.id}")
+    except YouBlockedUser:
+        await client.unblock_user(bot)
+        await client.send_message(bot, f"/search_id {user.id}")
+    await asyncio.sleep(1)
+
+    async for stalk in client.search_messages(bot, query="Name", limit=1):
+        if not stalk:
+            await message.edit_text("**Orang Ini Belum Pernah Mengganti Namanya**")
             return
-        else:
-            await lol.delete()
-            await opt.copy(chat)
+        elif stalk:
+            await message.edit(stalk.text)
+            await stalk.delete()
+
+    async for stalk in client.search_messages(bot, query="Username", limit=1):
+        if not stalk:
+            return
+        elif stalk:
+            await message.reply(stalk.text)
+            await stalk.delete()
 
 
 add_command_help(
     "sangmata",
     [
-        [".sg", "Reply to a user to find name history."],
+        [
+            f"{cmd}sg <reply/userid/username>",
+            "Untuk Mendapatkan Riwayat Nama Pengguna selama di telegram.",
+        ],
     ],
 )
