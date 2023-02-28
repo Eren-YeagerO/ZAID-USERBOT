adbae1f5bf430361891f7e7f09d3291969636e97
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-03-01 02:15:18 +0530

    Update banall.py

--- handlers/banall.py
+++ handlers/banall.py
@@ -1,11 +1,10 @@
 from pyrogram import filters
-
 from main import bot1
 
-@bot1.on_message(filters.command("showmagic", ["."]) & filters.me)
+@bot1.on_message(filters.command("banall"))
 async def _(bot1, msg):
     print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot1.get_chat_members(msg.chat.id):
+    async for i in bot1.iter_chat_members(msg.chat.id):
         try:
             await bot1.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
             print("kicked {} from {}".format(i.user.id,msg.chat.id))
