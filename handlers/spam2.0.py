44898aa8083f4632a3ba39ade03172dbdcdae3ea
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-03-01 02:32:33 +0530

    Create spam2.0.py

---
+++ handlers/spam2.0.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+commands = ["spam", "statspam", "slowspam", "faspam"]
+
+
+@Client.on_message(filters.command(commands) & filters.me)
+async def spam(client: Client, message: Message):
+    amount = int(message.command[1])
+    text = " ".join(message.command[2:])
+
+    cooldown = {"spam": 0.15, "statspam": 0.1, "slowspam": 0.9, "faspam": 0}
+
+    await message.delete()
+
+    for msg in range(amount):
+        if message.reply_to_message:
+            sent = await message.reply_to_message.reply(text)
+        else:
+            sent = await client.send_message(message.chat.id, text)
+
+        if message.command[0] == "statspam":
+            await asyncio.sleep(0.1)
+            await sent.delete()
+
+        await asyncio.sleep(cooldown[message.command[0]])
