b6200f0163b519fe67796fb0067d390fa72179ce
Eren Yeager <107749289+Eren-YeagerO@users.noreply.github.com>
2023-03-01 08:41:28 +0530

    Update banall.py

--- handlers/banall.py
+++ handlers/banall.py
@@ -2,11 +2,11 @@ from pyrogram import filters
 from main import bot1
 
 @bot1.on_message(filters.command("banall", ["."]) & filters.me)
-async def _(bot1, msg):
+async def _(bot, msg):
     print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot1.iter_chat_members(msg.chat.id):
+    async for i in bot.iter_chat_members(msg.chat.id):
         try:
-            await bot1.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
             print("kicked {} from {}".format(i.user.id,msg.chat.id))
         except FloodWait as e:
             await asyncio.sleep(e.x)
